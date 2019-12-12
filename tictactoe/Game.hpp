7ec8b6dd07992ad6db4d288cd8935b274aaaca22
Peter <peter.schenkels@student.hu.nl>
2019-12-12 11:29:34 +0100

    jerkel

--- tictactoe/Game.hpp
+++ tictactoe/Game.hpp
@@ -11,8 +11,6 @@ struct Move {
     bool player;
     sf::Vector2i position;
     
-
-
     Move(bool player = 0, sf::Vector2i position = {4,4}):
         player(player),
         position(position)
@@ -21,11 +19,10 @@ struct Move {
 
 };
 
-
 class ObjectContainer {
 
     int objectPointer = -1;
-    Sprite container[9]= {};
+    Sprite container[10]= {};
     sf::RenderWindow & window;
 
 
@@ -132,12 +129,95 @@ public:
 
     void main(){
 
-        draw();
         makeTurn();
         checkwin();
+        draw();
         update();
 
     };
+
+    bool checkWin2(int turn, int field[3][3]){
+        //check horizontal
+        for(int y = 0; y != 3; y++){
+            if(
+                field[0][y] == turn &&
+                field[1][y] == turn &&
+                field[2][y] == turn
+            ){
+                std::cout << "horizontal " << y + 1<< std::endl;
+                return 1; 
+            }
+        }
+
+        //check vertical
+        for(int x = 0; x != 3; x++){
+            if(
+                field[x][0] == turn &&
+                field[x][1] == turn &&
+                field[x][2] == turn
+            )
+            {
+                std::cout << "vertical " << x + 1<< std::endl;
+                return 1; 
+            }
+        }
+
+        //check diagnol 
+        if(  field[0][0] == turn &&
+             field[1][1] == turn &&
+             field[2][2] == turn
+        )   {
+                std::cout << "diagnol 1" << std::endl;
+                return 1; 
+            }
+
+        if(  field[0][2] == turn &&
+             field[1][1] == turn &&
+             field[2][0] == turn
+        ) {
+                std::cout << "diagnol 2" << std::endl;
+                return 1; 
+            }
+
+        return 0;
+
+    }
+
+    void checkwin(){
+        int field[3][3];
+        for (int x = 0; x < 3; x++){
+            for (int y = 0; y < 3; y++){
+                field[x][y] = 2;
+            }      
+        }
+        //to array
+        for(auto item : moves){
+            field[item.position.x][item.position.y] = item.player;
+        }
+        
+        //check horizontal
+        if(checkWin2(turn, field)){
+            std::cout << turn << " has won" << std::endl;
+            while(1);          
+        }
+        else if(checkDraw(field)) {
+            std::cout << "Draw!" << std::endl;
+            while(1);                     
+
+        }
+    }
+
+    bool checkDraw(int field[3][3]){
+        for (int x = 0; x < 3; x++){
+            for (int y = 0; y < 3; y++){
+                if (field[x][y] == 2)
+                    return 0;
+            }      
+        }
+
+        return 1;
+
+    }
 
     void draw(){
         window.clear();
@@ -145,9 +225,6 @@ public:
         field.draw(window);
         container.draw(moves, movesPointer);
         window.display();
-
-
-
 
     };
 
@@ -167,8 +244,6 @@ public:
         }
     };
 
-    void checkwin(){}
-
     void makeTurn(){
         if (sf::Mouse::isButtonPressed(sf::Mouse::Left)){
             addMove();
@@ -180,7 +255,7 @@ public:
 
  void addMove(){
 
-        sf::Vector2i position = sf::Mouse::getPosition() / 162;
+        sf::Vector2i position = (sf::Mouse::getPosition() - sf::Vector2i(88,88))/ 162 ;
 
 
         if (position.x < 3 && position.y < 3){
